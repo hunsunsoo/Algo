64db42294a6801eab2efb7ba7f32382ea27ea7b2
hunsunsoo <107869536+hunsunsoo@users.noreply.github.com>
2024-02-16 01:01:21 +0900

    [Silver III] Title: 두 수의 합, Time: 284 ms, Memory: 33756 KB -BaekjoonHub

---
+++ 백준/Silver III/3273. 두 수의 합/두 수의 합.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        int[] list = new int[N];
+        HashSet<Integer> set = new HashSet<>();
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i=0; i<N; i++) {
+            list[i] = Integer.parseInt(st.nextToken());
+            set.add(list[i]);
+        }
+
+        int X = Integer.parseInt(br.readLine());
+        int cnt = 0;
+        for (int i=0; i<N; i++) {
+            if (set.contains(X-list[i])) {
+                cnt++;
+            }
+
+        }
+
+        System.out.println(cnt/2);
+    }
+}
