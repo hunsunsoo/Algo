6038a0dc946e94f8f3fab54e278714b1b36cbc54
hunsunsoo <107869536+hunsunsoo@users.noreply.github.com>
2024-03-04 14:27:56 +0900

    [Gold IV] Title: 여왕벌, Time: 2580 ms, Memory: 336416 KB, Score: 100 point -BaekjoonHub

--- 백준/Gold/10836. 여왕벌/여왕벌.java
+++ 백준/Gold/10836. 여왕벌/여왕벌.java
@@ -8,7 +8,6 @@ public class Main {
         StringTokenizer st = new StringTokenizer(br.readLine());
         int M = Integer.parseInt(st.nextToken());
         int[][] map = new int[M][M];
-        int[][] temp = new int[M][M];
         int N = Integer.parseInt(st.nextToken());
         int[][] list = new int[N][3];
 
@@ -25,50 +24,32 @@ public class Main {
             list[i][2] = Integer.parseInt(st.nextToken());
         }
 
-        // 애벌레 성장
+        int[] temp = new int[2*M-1];
         for (int d=0; d<N; d++) { // 날짜 증가
-            int plus = 0;
-            // 왼쪽 아래 -> 위
-            for (int i=M-1; i>=0; i--) {
-                while (true) {
-                    if (list[d][plus] > 0) {
-                        map[i][0] += plus;
-                        list[d][plus]--;
-                        break;
-                    } else {
-                        plus++;
-                    }
-                }
+            // 1 증가
+            for (int i=list[d][0]; i<list[d][1]+list[d][0]; i++) {
+                temp[i] += 1;
             }
 
-            // 맨윗줄
-            for (int j=1; j<M; j++) {
-                while (true) {
-                    if (list[d][plus] > 0) {
-                        map[0][j] += plus;
-                        list[d][plus]--;
-                        break;
-                    } else {
-                        plus++;
-                    }
-                }
+            // 2 증가
+            for (int i=list[d][1]+list[d][0]; i<list[d][2]+list[d][1]+list[d][0]; i++) {
+                temp[i] += 2;
             }
-
-            // 가운데
-//            for (int i=1; i<M; i++) {
-//                for (int j=1; j<M; j++) {
-//                    int max = Math.max(Math.max(temp[i][j-1], temp[i-1][j-1]), temp[i-1][j]);
-//                    temp[i][j] = max;
-//                }
-//            }
-
-            // map + temp
-//            for (int i=0; i<M; i++) {
-//                for (int j=0; j<M; j++) {
-//                    map[i][j] += temp[i][j];
-//                }
-//            }
         }
+
+        int idx = 0;
+        // 왼쪽 아래 -> 위
+        for (int i=M-1; i>=0; i--) {
+            map[i][0] = temp[idx]+1;
+            idx++;
+        }
+
+        for (int j=1; j<M; j++) {
+            map[0][j] = temp[idx]+1;
+            idx++;
+        }
+
+        // 가운데
         for (int i=1; i<M; i++) {
             for (int j=1; j<M; j++) {
                 map[i][j] = map[i-1][j];
@@ -76,11 +57,13 @@ public class Main {
         }
 
         // 출력
+        StringBuilder sb = new StringBuilder();
         for (int i=0; i<M; i++) {
             for (int j=0; j<M; j++) {
-                System.out.print(map[i][j]+" ");
+                sb.append(map[i][j]+" ");
             }
-            System.out.println();
+            sb.append("\n");
         }
+        System.out.println(sb);
     }
 }
