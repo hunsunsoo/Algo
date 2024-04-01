807d33e1114831a1b4bd2dea65fe62db10f356e3
hunsunsoo <107869536+hunsunsoo@users.noreply.github.com>
2024-04-01 12:21:39 +0900

    [Silver I] Title: IOIOI, Time: 204 ms, Memory: 24644 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/5525. IOIOI/IOIOI.java
+++ 백준/Silver/5525. IOIOI/IOIOI.java
@@ -8,18 +8,28 @@ public class Main {
         int N = Integer.parseInt(br.readLine());
         int M = Integer.parseInt(br.readLine());
         String s = br.readLine();
-        char[] list = {'I', 'O'};
+
+        int[] list = new int[M];
+        if (s.charAt(0) == 'I') list[0] = 1;
 
         int result = 0;
-        for (int i=0; i<M-2*N; i++) {
-            int cnt = 0;
-            for (int j=0; j<2*N+1; j++) {
-                int num = j%2;
-                if (s.charAt(i+j) == list[num]) cnt++;
-                else break;
+        for (int i=1; i<M; i++) {
+            if (list[i-1] == 0) {
+                if (s.charAt(i) == 'I') list[i] = list[i-1]+1;
+            } else if (list[i-1]%2 == 0) { // 직전값 O
+                if (s.charAt(i) == 'I') list[i] = list[i-1]+1;
+                else { // 카운트
+                    if ((list[i-1]-1)/2 + 1 - N > 0) result += (list[i-1]-1)/2 + 1 - N;
+                }
+            } else { // 직전값 I
+                if (s.charAt(i) == 'O') list[i] = list[i-1]+1;
+                else { // 카운트
+                    if ((list[i-1]-1)/2 + 1 - N > 0) result += (list[i-1]-1)/2 + 1 - N;
+                    list[i] = 1;
+                }
             }
-            if (cnt == 2*N+1) result++;
         }
+        if ((list[M-1]-1)/2 + 1 - N > 0) result += (list[M-1]-1)/2 + 1 - N;
 
         System.out.println(result);
     }
